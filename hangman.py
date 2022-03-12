ec9a31b632c1a683437716e9054ddb319e30cef7
gekogit <64784223+gekogit@users.noreply.github.com>
2022-03-12 16:39:57 +0100

    Add files via upload
    ready to test for my teacher

---
+++ hangman.py
@@ -0,0 +1,37 @@
+"""
+Hangman Computer random word comes from list. On the screen comes shadowed ( ----).
+User can guess whole word or gives a letter. If letter is in the word show up with the comment “ Letter in the word”
+if not “Not in the word”. After certain attempts game finished.
+"""
+import random
+
+def word_random():
+    words_base = ['Sarah', 'Anna', 'Julia', 'Laura', 'Lena', 'Hannah', 'Lisa', 'Katharina', 'Leonie', 'Vanessa']
+    return words_base[random.randrange(0, len(words_base))]
+
+
+repeat_number = 10
+
+computer_words = word_random()
+print('for those who likes to cheat:', computer_words)
+user_shadow_word = '-' * len(computer_words)
+print('\n\n\nGuess the word (ps.cap letter sensitive, max 10 attempts):')
+print(user_shadow_word)
+for n in range(repeat_number):
+    user_letter = input('Your letter:')
+    if user_letter == computer_words:
+        print('Well done')
+        break
+    for letter_index in range(len(computer_words)):
+        letter_in_word = False
+        if computer_words[letter_index] == user_letter:
+            user_shadow_word = user_shadow_word[:letter_index] + user_letter + user_shadow_word[letter_index + 1:]
+            letter_in_word = True
+    if letter_in_word:
+        print(f'Bravo {user_letter} is in the word.')
+    print(user_shadow_word)
+    if user_shadow_word == computer_words:
+        print('Well done')
+        break
+    if n == repeat_number:
+        print('Ups... Try again next time.')
